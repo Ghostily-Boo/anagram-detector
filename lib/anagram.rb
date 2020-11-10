d75412b9f1779e4e4174c935d9b7768e994af2d9
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-10 16:04:47 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word, :array
+
+    def initialize(word)
+        @word = word.split("").sort
+    end
+    
+    def match(array)
+        tada = []
+        array.each do |array|
+            if array.split("").sort == @word
+                tada << array
+            end
+        end
+        tada
+    end
+
+
+
+            
+end
